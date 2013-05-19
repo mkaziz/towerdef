008c5c1b7206755912a9492a5d0a987264993f5b
Khalid <kaziz@u.northwestern.edu>
2013-05-19 02:07:25 -0500

    Added a menu and start button with hover functionality

--- towerdef.js
+++ towerdef.js
@@ -14,13 +14,107 @@ goog.require('lime.animation.ScaleTo');
 goog.require('lime.animation.MoveTo');
 
 
+GameObjects = {
+    
+        player : {
+            gymHealth : 100,
+            army : []
+        }
+    
+    }
+
+towerdef.addHoverListener = function() {
+    /**
+    Test for correctly dispatching mouseover/mouseout for LimeJS objects
+    Usage: scene.listenOverOut(shape,function(e){ console.log('over'); }, function(e){ console.log('out'); });
+    Advice welcome about how to have the same result with more LimeJS/Closure style API.
+    */
+    lime.Scene.prototype.listenOverOut = (function(){
+     
+        var moveHandler = function(e){
+            for(var i in this.registeredOverOut_){
+                var item = this.registeredOverOut_[i];
+                var shape = item[0];
+                if(!shape.inTree_) 
+                    continue;
+                var insideShape = shape.hitTest(e);
+                if(!shape.insideShape_ && insideShape && goog.isFunction(item[1])){
+                    item[1].call(shape,e);
+                }
+                if(shape.insideShape_ && !insideShape && goog.isFunction(item[2])){
+                    item[2].call(shape,e);
+                }
+                shape.insideShape_ = insideShape;
+            }
+        };
+         
+        return function(shape,over,out){
+            if(shape==this) return; //scene itself is always full
+         
+            if(!this.registeredOverOut_){
+            this.registeredOverOut_ = {};
+            }
+         
+            var uuid = goog.getUid(shape);
+             
+            if(!over && !out) //clear if empty
+            delete this.registeredOverOut_[uuid];
+         
+            if(!this.isListeningOverOut_){
+                goog.events.listen(this,"mousemove",moveHandler,false,this);
+                this.isListeningOverOut_ = true;
+            }
+         
+            this.registeredOverOut_[uuid] = [shape,over,out];
+        }
+    })();
+    
+}
+    
+
+    
+towerdef.initializeMenuScene = function (director) {
+    var menuScene = new lime.Scene();
+    director.replaceScene(menuScene);
+    
+    var layer = new lime.Layer().setPosition(0,0).setRenderer(lime.Renderer.CANVAS).setAnchorPoint(0,0);
+    
+    var background = new lime.Sprite().setSize(900,506).setFill("background.png").setPosition(0,0).setAnchorPoint(0,0);
+    var logo = new lime.Sprite().setSize(700,256).setFill("pokemon-logo.png").setPosition(450,153).setAnchorPoint(0.5,0.5).setScale(0.6,0.6);
+    var start = new lime.Sprite().setSize(330,330).setFill("start.png").setPosition(450,350).setAnchorPoint(0.5,0.5).setScale(0.6,0.6);
+    
+    layer.appendChild(background);
+    layer.appendChild(logo);
+    layer.appendChild(start);
+    
+    menuScene.appendChild(layer);
+    
+    menuScene.listenOverOut(start,function(e){
+
+        //animate
+        start.runAction(new lime.animation.ScaleTo(0.7).setDuration(.05));
+        
+        console.log("hover in");
+    }, function() {
+        console.log("hover out");
+        start.runAction(new lime.animation.ScaleTo(0.6).setDuration(.05));
+        
+    });
+    
+}
+
 towerdef.start = function(){          
     var director = new lime.Director(document.body,900,506);     
     director.makeMobileWebAppCapable();     
     director.setDisplayFPS(true);          
     
+    towerdef.addHoverListener();
+    towerdef.initializeMenuScene(director);
+    
+    /*
     var mapScene = new lime.Scene();              
     director.replaceScene(mapScene); 
+    
     
     var mapLayer = new lime.Layer().setPosition(0,0).setRenderer(lime.Renderer.CANVAS).setAnchorPoint(0,0);
     var gameMap = new lime.Sprite().setSize(900,506).setFill("background.png").setPosition(0,0).setAnchorPoint(0,0);
@@ -28,17 +122,26 @@ towerdef.start = function(){
     mapLayer.appendChild(gameMap);
     mapScene.appendChild(mapLayer);
     
+    
     var pikachu = new lime.Sprite().setSize(19,19).setFill("Pikachu_1.png").setPosition(0,0).setAnchorPoint(0,0);
     
     mapLayer.appendChild(pikachu);
     
+    
+    lime.scheduleManager.scheduleWithDelay(function (dt) {
+        var pikachu = new lime.Sprite().setSize(19,19).setFill("Pikachu_1.png").setPosition(0,0).setAnchorPoint(0,0);
+        this.appendChild(pikachu);
+        var movement = new lime.animation.MoveTo(Math.floor((Math.random()*500)+1),Math.floor((Math.random()*500)+1)).setDuration(1.0);
+        pikachu.runAction(movement);
+    }, mapLayer, 10000);
+    */
+    /*
     goog.events.listen(gameMap,['mousedown','touchstart'],function(e) {
-        var movement = new lime.animation.MoveTo(e.position.x,e.position.y).setDuration(1);
+        var movement = new lime.animation.MoveTo(e.position.x,e.position.y).setDuration(0.1);
         pikachu.runAction(movement);
     });
-    
+    * */
 }
-
 /*
 // entrypoint
 towerdef.start = function(){
@@ -95,6 +198,7 @@ towerdef.start = function(){
 
 }
 */
+
 
 //this is required for outside access after code is compiled in ADVANCED_COMPILATIONS mode
 goog.exportSymbol('towerdef.start', towerdef.start);
